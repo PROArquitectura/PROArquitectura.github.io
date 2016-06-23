591c7ca0a993499d6e695e9127f58f191a01020d
jsvelasquezv <juseve200@gmail.com>
2016-06-23 17:34:04 -0500

    First Upload

---
+++ js/hover-menu.js
@@ -0,0 +1,44 @@
+$('#belmonte').mouseenter(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/belmonte.png")');
+  }
+);
+$('#belmonte').mouseleave(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/proyectos_fondo.png")');
+  }
+);
+
+$('#lindaraja').mouseenter(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/lindaraja.png")');
+  }
+);
+$('#lindaraja').mouseleave(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/proyectos_fondo.png")');
+  }
+);
+
+$('#castilla').mouseenter(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/castillanueva.png")');
+  }
+);
+$('#castilla').mouseleave(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/proyectos_fondo.png")');
+  }
+);
+
+$('#portafolio').mouseenter(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/portafolio.png")');
+  }
+);
+$('#portafolio').mouseleave(
+  function () {
+    $('#section1').css("background-image", 'url("../images/landing/proyectos_fondo.png")');
+  }
+);
+
